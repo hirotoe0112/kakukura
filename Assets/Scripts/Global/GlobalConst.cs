a03ccb9860ede91bd7a27c830d2854ed2a327df7
hirotoe0112 <hirotoe0112@gmail.com>
2021-01-11 16:00:35 +0900

    タイトルアニメーション修正

---
+++ Assets/Scripts/Global/GlobalConst.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalConst
+{
+    public const string SCENE_TITLE = "TitleScene";
+    public const string SCENE_GAME = "GameScene";
+}
---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    /// <summary>
+    /// ゲーム開始
+    /// </summary>
+    public void GameStart()
+    {
+        SceneManager.LoadScene(GlobalConst.SCENE_GAME);
+    }
+
+    /// <summary>
+    /// ランキング表示
+    /// </summary>
+    public void Ranking()
+    {
+
+    }
+
+    /// <summary>
+    /// 図鑑
+    /// </summary>
+    public void DisplayBook()
+    {
+
+    }
+
+    /// <summary>
+    /// 遊び方
+    /// </summary>
+    public void Tutorial()
+    {
+
+    }
+
+    /// <summary>
+    /// 設定
+    /// </summary>
+    public void Settings()
+    {
+
+    }
+}
